cb079bd382869b038a7d10d2a4118073ab939e28
Gui Iribarren <gui@altermundi.net>
2023-09-26 15:42:06 +0200

    (monkeypatch) add `local` env

--- src/util/constants.ts
+++ src/util/constants.ts
@@ -1,27 +1,32 @@
 export const API_URL = {
+  local: 'http://localhost:8081/v2',
   dev: 'https://api-dev.vocdoni.net/v2',
   stg: 'https://api-stg.vocdoni.net/v2',
   prod: 'https://api.vocdoni.net/v2',
 };
 
 export const EXPLORER_URL = {
+  local: 'http://localhost:8082',
   dev: 'https://dev.explorer.vote',
   stg: 'https://stg.explorer.vote',
   prod: 'https://explorer.vote',
 };
 
 export const CENSUS3_URL = {
+  local: 'http://localhost:8083/api',
   dev: 'https://census3.dev.vocdoni.net/api',
   stg: 'https://census3.stg.vocdoni.net/api',
   prod: 'https://census3.vocdoni.net/api',
 };
 
 export const FAUCET_URL = {
+  local: 'http://localhost:8081/v2/faucet/local',
   dev: 'https://faucet-azeno.vocdoni.net/faucet/vocdoni/dev',
   stg: 'https://faucet-azeno.vocdoni.net/faucet/vocdoni/stage',
 };
 
 export const FAUCET_AUTH_TOKEN = {
+  local: '158a58ba-bd3e-479e-b230-2814a34fae8f',
   dev: '158a58ba-bd3e-479e-b230-2814a34fae8f',
   stg: '158a58ba-bd3e-479e-b230-2814a34fae8f',
 };
